bebb60e2239cb93ef3fd01b3a50e98d2a504aa6a
James Huston <jamzhuston@gmail.com>
2024-05-06 13:35:11 -0600

    added a list instead of file initially, and added a way to save the list to a file

--- todolist/todo.py
+++ todolist/todo.py
@@ -1,13 +1,51 @@
 # Author: James Huston
 # Email:  jamzhuston@gmail.com
 # Description:
-# This is a simple calculator project
-# the calculator will add, multiply, divide, and subtract
+# 
+# 
 
+# Tasks:
+# write a todo list in a list of tuples
+# create a file
+# read a file (must make sure file exists)
+# write a file ()
+# remove items
+# add items
+# change priority
+# add extension to todos
+# add ui
+# add stages (todo, doing, done)
+     
 
 from datetime import date
+import copy
+# create a todo list with a list and a tuple
+# print the rules
+# on quit ask the user if they would like to save their todolist
 
-createLoadAnswer = input("Create or Load a ToDo list: [C/L]").lower()
+class ToDo:
+    todaysDate = date.today()
+    task = "no task"
+    time = 0
+    priority = 0
+
+
+
+
+
+
+
+
+
+
+def saveToDoList(todoList):
+    name = input("What would you like to name your ToDo list: ")
+    f = open("%s.csv" %name, 'x')
+    for x in todoList:
+        f.write(f'{x.todaysDate},{x.task},{x.priority},{x.time}')
+
+
+
 
 
 def menu():
@@ -20,36 +58,28 @@ def menu():
                 \nQuit --------------------> [Q]""")
 
 
-def addTaskToFile(file,date,time,priority,task):
-    print(0)
-    
-def createTasks(file):
+def createTasks():
     answer = "y"
+    todo = ToDo()
+    todoList = []
     while answer != "n":
-        task = input("What is the task: ").lower()
-        date = date.today()
-        time = input("How long should this take you? ")
-        priority = input("""How Important is this task?\n
+        todo.task = input("What is the task: ").lower()
+        todo.todaysDate = date.today()
+        todo.time = input("How long should this take you? ")
+        todo.priority = input("""How Important is this task?\n
                             Nice To Have --> [1]\n
                             Important    --> [2]\n
                             Crucial      --> [3]""")
-        addTaskToFile(filemanip,date,time,priority,task)
+        todoList.append(copy.copy(todo))
         answer = input("Would you like to contiue adding add tasks [Y/N]").lower()
+    return todoList
 
+createLoadAnswer = input("Create or Load a ToDo list: [C/L]").lower()
 
 if createLoadAnswer == "c":
-    name = input("What would you like to name your ToDo list: ")
-    filemanip = open("%s.csv" %name, 'x')
     addTasksAnswer = input("Would you like to add tasks [Y/N]").lower()
     if addTasksAnswer == "y":
-        createTasks(filemanip)
+        createTasks()
 else:
     print(0)
-    # read a file (must make sure file exists)
-    # write a file ()
-    # remove items
-    # add items
-    # change priority
-    # add extension to todos
-    # add ui
     
